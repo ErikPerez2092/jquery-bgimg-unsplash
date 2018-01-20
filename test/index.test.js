1d5f65387d43634ad55728ab3510fc88b2169d0a
Erik <erik.perez@ixiel.com>
2018-01-20 15:19:29 -0600

    test: add code coverage

--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,25 @@
 var expect = require('expect.js');
+var jsdom = require('jsdom');
 
-describe('Calculadora', function() {
-  it('sumar dos números', function(){
-    expect(2+2).to.be(4);
+var dom = new jsdom.JSDOM('<html><body><section></section></body></html>');
+var $ = global.jQuery = require('jquery')(dom.window);
+
+require('../src');
+
+
+
+describe('jquery-bgimag-unsplash', function() {
+  var $section;
+  beforeEach(function () {
+    $section = $('section');
+    $section.bgimgUnsplash();
+  });
+
+  it('should have a width of 100%', function(){
+    expect($section.css('width')).to.be('100%');
+  });
+
+  it('should have a minheight of 800px', function(){
+    expect($section.css('minHeight')).to.be('800px');
   });
 });
